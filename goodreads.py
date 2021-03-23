08d2db50f2058dde34c5bfbf1eb0d62b67316645
berkelem <42berkeley@cua.edu>
2021-03-24 00:02:13 +0100

    Add Other Reviews

--- goodreads.py
+++ goodreads.py
@@ -50,17 +50,28 @@ class Goodreads(WebLoader):
 
     def __init__(self, n):
         super().__init__()
-        self.url = 'https://www.goodreads.com/review/list/6157820-matthew?utf8=%E2%9C%93&utf8=%E2%9C%93&order=d&sort=date_read&view=reviews&title=matthew&per_page=750'
+        # self.url = 'https://www.goodreads.com/review/list/6157820-matthew?utf8=%E2%9C%93&utf8=%E2%9C%93&order=d&sort=date_read&view=reviews&title=matthew&per_page=750'
         self.browser = self.init_browser()
-        self.load_page()
         self.n = n
-        self.soup = BeautifulSoup(self.browser.page_source, 'html.parser')
+        self.soup = None
+        self.other_reviewers = ["1923320-jessica", "19090274-zbigniew-zdziarski", "6100646-brian-clegg"]
 
-    def load_page(self):
+
+        #
+        # "https://www.goodreads.com/review/list/1923320-jessica?utf8=%E2%9C%93&order=d&sort=review&view=reviews&title=jessica&per_page=infinite"
+        # "https://www.goodreads.com/review/list/19090274-zbigniew-zdziarski?utf8=%E2%9C%93&order=d&sort=review&view=reviews&title=zbigniew-zdziarski&per_page=infinite"
+        # "https://www.goodreads.com/review/list/6100646-brian-clegg?utf8=%E2%9C%93&title=brian-clegg&per_page=infinite"
+
+    def construct_url(self, user_id):
+        user_num, user_name = user_id.split("-", 1)
+        url = "https://www.goodreads.com/review/list/{}?utf8=%E2%9C%93&title={}&per_page=infinite".format(user_id, user_name)
+        return url, user_name
+
+    def load_page(self, url):
         incomplete = True
         while incomplete:
             try:
-                self.browser.get(self.url)
+                self.browser.get(url)
                 incomplete = False
             except WebDriverException:
                 continue
@@ -79,36 +90,62 @@ class Goodreads(WebLoader):
         full_book_url = "https://www.goodreads.com{}".format(book_url)
         return full_book_url
 
-    def get_reviews(self):
+    def format_name(self, name):
+        clean_name = name.replace("-", " ").title()
+        return clean_name
+
+    def get_my_reviews(self):
+        url, name = self.construct_url("6157820-matthew")
+        clean_name = self.format_name(name)
+        reviews = self.get_reviews(url)
+        return reviews, clean_name
+
+    def get_other_reviews(self):
+        randidx = random.randint(0, len(self.other_reviewers) - 1)
+        url, name = self.construct_url(self.other_reviewers[randidx])
+        clean_name = self.format_name(name)
+        reviews = self.get_reviews(url)
+        return reviews, clean_name
+
+    def get_reviews(self, url):
+        self.load_page(url)
+        self.soup = BeautifulSoup(self.browser.page_source, 'html.parser')
         book_reviews = self.find_reviews_in_html()
         num_reviews = len(book_reviews)
         rand_ints = random.sample(range(num_reviews), self.n)
         reviews = []
         for i in rand_ints:
             job_elem = book_reviews[i]
-            title, review = self.extract_review(job_elem)
+            title, author, review = self.extract_review(job_elem)
             while review == "None":
                 j = np.random.randint(0, len(book_reviews))
                 if j in rand_ints:
                     continue
                 job_elem = book_reviews[j]
-                title, review = self.extract_review(job_elem)
+                title, author, review = self.extract_review(job_elem)
             full_book_url = self.get_book_url(job_elem)
             clean_title = title.text.split("\n")[1].strip()
-            reviews.append((clean_title, review, full_book_url))
+            clean_author = " ".join(author.text.split("author ")[1].strip().split(", ")[::-1])
+            reviews.append((clean_title, clean_author, review, full_book_url))
         return reviews
 
     @staticmethod
     def extract_review(element):
         title = element.find("td", class_="field title")
+        author = element.find("td", class_="field author")
         review = element.find("span", {"id": lambda l: l and l.startswith('freeTextreview')})
         review = re.sub("<span.*?>", "", str(review))
         review = re.sub("</span>", "", str(review))
-        return title, review
+        return title, author, review
 
-    def create_html_message(self, review_tuple):
-        title, review, url = review_tuple
-        html = """<p><a href="{}">{}</a><br><br>
-                     Review: {}<br>       
-                  </p> """.format(url, title, review)
+    def create_html_message(self, review_tuple, header=None):
+        title, author, review, url = review_tuple
+        html = """<p>
+                    <h1>
+                      {}
+                    </h1>
+                    <a href="{}">{}</a> by {}
+                    <br><br>
+                    {}<br>       
+                  </p> """.format(header, url, title, author, review)
         return html
--- run_digest.py
+++ run_digest.py
@@ -8,10 +8,16 @@ if __name__ == "__main__":
     twttr_scraper = twitter.TwitterCrawler(3)
     email = sendemail.DigestEmail()
 
-    reviews = gdr_reader.get_reviews()
+    my_reviews, my_name = gdr_reader.get_my_reviews()
+    other_reviews, other_name = gdr_reader.get_other_reviews()
     html_message = """"""
-    for review in reviews:
-        html_message += gdr_reader.create_html_message(review)
+    for review in my_reviews:
+        header = "Review by {}".format(my_name)
+        html_message += gdr_reader.create_html_message(review, header=header)
+
+    for review in other_reviews:
+        header = "Review by {}".format(other_name)
+        html_message += gdr_reader.create_html_message(review, header=header)
 
     tweet_likes_html = twttr_scraper.get_n_likes()
     html_message += tweet_likes_html
